3ca62a7c7ec6e42d540184859c4c6c81fed4cf67
nouraalj <aljane92noura@gmail.com>
2023-12-09 16:40:01 +0100

    ?

--- TME6/src/fight.cpp
+++ TME6/src/fight.cpp
@@ -6,16 +6,16 @@
 #include "rsleep.cpp"
 
 int pv = 3;
-void handler(int){
+void handler(int) {
     pv--;
     std::cout << "Nombre de points de vie restants : " << pv << " pour :" << getpid()<< std::endl;
-    if (pv == 0){
+    if (pv == 0) {
         std::cout << getpid() << " est mort !" << std::endl;
         exit(1);
     }
 }
-void attaque (pid_t adversaire){
-    struct sigaction sa={0};
+void attaque (pid_t adversaire) {
+    struct sigaction sa = {0};
     sigset_t mask;
     sigfillset(&mask);
     sigdelset(&mask, SIGINT);
@@ -23,32 +23,32 @@ void attaque (pid_t adversaire){
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
     sigaction(SIGINT, &sa, NULL);
-    if (kill(adversaire, SIGINT) == -1){
+    if (kill(adversaire, SIGINT) == -1) {
 
         exit(0);
     }
     randsleep();
 }
 
-void defense(){
+void defense() {
     signal(SIGINT, SIG_IGN);
     randsleep();
 }
 
-void combat(pid_t adversaire){
-    while(true){
+void combat(pid_t adversaire) {
+    while(true) {
         attaque(adversaire);
         defense();
     }
 }
 
-int main(){
+int main() {
     pid_t vador = getpid(); //père
     pid_t luke = fork(); //fils
     std::cout << "Dark Vador est : " << vador << std::endl;
     std::cout << "Luke est : " << luke << std::endl;
-    if (luke == 0){
-    combat(vador);
+    if (luke == 0) {
+        combat(vador);
     } else combat(luke);
     return 0;
 }
--- TME6/src/fork_exo.cpp
+++ TME6/src/fork_exo.cpp
@@ -5,34 +5,33 @@
 #include <sys/wait.h>
 int main () {
 	const int N = 3;
-	int nb_child = 0;
+	int nb_fils = 0;
 	std::cout << "main pid=" << getpid() << std::endl;
 	pid_t pidf = fork();
 	for (int i=1, j=N; i<=N && j==N && pidf==0 ; i++ ) { // fils de main
 		if (pidf != 0){ //père
-			nb_child++;
+			nb_fils++;
 			break;
 		} else { //fils
-			nb_child = 0;
+			nb_fils = 0;
 		}
 		std::cout << " i:j " << i << ":" << j << std::endl;
-
 		for (int k=1; k<=i && j==N ; k++) { // petit-fils de main
 			if ( fork() == 0) {
-				nb_child =0;
+				nb_fils =0;
 				j=0;
 				std::cout << " k:j " << k << ":" << j << std::endl;
-			} else nb_child++;
+			} else nb_fils++;
 		}
 
 	}
-	for (int j =0; j< nb_child; j++){
-		wait(NULL);
-		if (wait(NULL) == -1){
-			std::cerr << "Trop de wait \n" << std::endl;
+	for (int j =0; j< nb_fils; j++){
+		int i  = wait(NULL);
+		if (i == -1){
+			std::cerr << "Trop de wait :(" << std::endl;
 		}
 	}
-	if (wait(NULL) != -1)
-		std::cerr << "Il en reste \n";
+	if (wait(NULL) == -1)
+		std::cout << "J'ai fini d'attendre mes fils :)" << std::endl;
 	return 0;
 }
